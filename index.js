e1bda7df91fa5bcee6bf369c39e2de15c42a970d
YuTao <tao.yu@medcloud.cn>
2020-08-19 18:55:35 +0800

    docs: 修改文案并删除冗余代码

--- index.js
+++ index.js
@@ -3,22 +3,6 @@ class DeleteSourceMapWebpackPlugin {
   }
 
   apply (compiler) {
-    // let countMatchAssets = 0;
-    // compiler.plugin('after-compile', (compilation, cb) => {
-    //     Object.keys(compilation.assets).filter((key) => {
-    //       return /\.js$/.test(key);
-    //     })
-    //     .forEach((key) => {
-    //       countMatchAssets += 1;
-    //       let asset = compilation.assets[key];
-    //       let source = asset.source().replace(/# sourceMappingURL=(.*\.map)/g, '# $1');
-    //       compilation.assets[key] = Object.assign(asset, {
-    //         source: function () { return source }
-    //       });
-    //     });
-    //     cb()
-    // })
-
     compiler.plugin('done', (stats) => {
       const fs = require('fs')
       let countMatchMapAssets = 0
@@ -29,7 +13,6 @@ class DeleteSourceMapWebpackPlugin {
         const { existsAt } = stats.compilation.assets[name]
         fs.unlinkSync(existsAt)
       })
-      // console.log(`⭐⭐⭐removed source map url: ${countMatchAssets} asset(s) processed`);
       console.log(`⭐⭐⭐deleted map file: ${countMatchMapAssets} asset(s) processed`);
     })
   }
