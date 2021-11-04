e72f2ec0e723fd55ff0eae89f03fbe2e388a7fcb
TheDoctor0 <dawid.janik95@gmail.com>
2021-11-04 08:48:51 +0100

    Upgrade to Webpack 5.

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-class DeleteSourceMapWebpackPlugin {
-  constructor () {
-  }
+class DeleteSourceMapsWebpackPlugin {
+  constructor () {}
 
   apply (compiler) {
-    compiler.plugin('done', (stats) => {
-      const fs = require('fs')
-      let countMatchMapAssets = 0
-      Object.keys(stats.compilation.assets)
-      .filter(name => /\.js\.map$/.test(name))
-      .forEach((name) => {
-        countMatchMapAssets += 1
-        const { existsAt } = stats.compilation.assets[name]
-        if (existsAt) {
-         fs.unlinkSync(existsAt) 
-        }
-      })
+    compiler.hooks.done.tap('DeleteSourceMapsWebpackPlugin', (stats) => {
+      const fs = require('fs');
+      let countMatchMapAssets = 0;
+
+      Object
+        .keys(stats.compilation.assets)
+        .filter(name => /\.js\.map$/.test(name))
+        .forEach((name) => {
+          countMatchMapAssets += 1;
+          const { existsAt } = stats.compilation.assets[name];
+
+          if (existsAt) {
+           fs.unlinkSync(existsAt);
+          }
+        })
+
       console.log(`⭐⭐⭐deleted map file: ${countMatchMapAssets} asset(s) processed`);
     })
   }
 }
 
-module.exports = DeleteSourceMapWebpackPlugin
+module.exports = DeleteSourceMapsWebpackPlugin;
